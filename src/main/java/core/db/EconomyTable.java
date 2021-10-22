0e49a3fbc98237ca708e923353a6435919c8642b
wsbrenk <wolfgang.schneider@zissener-weg-brenk.de>
2021-10-22 12:56:28 +0200

    * release action shall run on release branches too (#1177)
    * #1175 fix DBManager.instance init parentFolder (#1176)
    
    * #1175 fix DBManager.instance init parentFolder
    
    * #1175 error message shows db folder name
    
    * fix backward compatibility to HO3 updater (#1225)
    
    * #1224 latestbeta.html merge to master
    
    * Edit substitutions (#1247)
    
    * Edit substitutions
    
    * Only 0..45 45..90 ET in match ratings
    
    * #1244 (#1248)
    
    * #1244 fix bug in TeamsRatingPanel.setBarValue
    
    * latestbeta.html->38020
    
    * #1245 (#1249)
    
    * #1245 FutureTrainingManager.previewPlayer uses local instead of global season
    
    * latestbeta.html ->3821
    
    * #1226 StaffTable.getStaffByHrfId do not query empty database
    
    Co-authored-by: Goran Stefanovic <64475678+steffanots@users.noreply.github.com>

--- src/main/java/core/db/EconomyTable.java
+++ src/main/java/core/db/EconomyTable.java
@@ -8,7 +8,7 @@ import java.sql.Types;
 public final class EconomyTable extends AbstractTable {
 
 	public final static String TABLENAME = "ECONOMY";
-	
+
 	EconomyTable(JDBCAdapter  adapter){
 		super(TABLENAME,adapter);
 	}
